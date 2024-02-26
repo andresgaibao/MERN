e6ca027a44b0a5445485c66ac2d11495b03e34df
Andres <gaibaoandres@gmail.com>
2024-02-26 10:37:47 -0500

    CRUD-AVANCES_PART_4

---
+++ client/src/pages/Register.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export const Register = () => {
+  return (
+    <>
+        <form action="">
+            <input type="text" name="" id="" />
+            <input type="email" name="" id="" />
+            <input type="password" name="" id="" />
+            <button type="submit" className='rounded-full'>Register</button>
+        </form>
+    </>
+  )
+}
